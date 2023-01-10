c2c4da70d05c6a3fb72235d44c3641278f731a10
Yumenotimata <kouyayamaoka@gmail.com>
2023-01-10 14:25:04 +0900

    write makefile

--- src/compiler.c
+++ src/compiler.c
@@ -1,7 +1,14 @@
 #include "../include/compiler.h"
 
-int main(void)
+int main(int argc,char *argv[])
 {
     FILE *fp;
+    if(argv[1]!=NULL)
+    {//
+        
+    }else
+    {
+        
+    }
     return 0;
 }

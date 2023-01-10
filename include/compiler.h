40e3b9d1821867d0e1cb455342886684547da2cf
Yumenotimata <kouyayamaoka@gmail.com>
2023-01-10 14:16:13 +0900

    add makefile

---
+++ include/compiler.h
@@ -0,0 +1,21 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/token.h"
+#include "../include/tokenizer.h"
+#include "../include/tools/tokenizerTools.h"
+#include "../include/cabinet.h"
+#include "../include/parser.h"
+#include "../include/tools/parseTools.h"
+#include "../include/codeGenerator.h"
+#include "../include/tools/codeGeneratorTools.h"
+#include "../include/typeList.h"
+
+#endif
